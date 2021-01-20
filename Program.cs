6f0858ca1b2bb43d9b54c4bdff5eb853be96403e
wellsbunk5 <71154493+wellsbunk5@users.noreply.github.com>
2021-01-20 14:05:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// Honestly not one hundred percent sure but I think this is giving the project a name and then calling a class
+// with the 'Main' method which starts the program. It then basically says to use the startup.cs page
+namespace Assignment1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,60 @@
+﻿// declare and assign a variable for the submit button
+var submit;
+submit = document.getElementById("submit");
+
+// when submit button is clicked do this function
+submit.addEventListener("click", function () {
+    // declare and assign values from the form
+    var assignments = document.getElementById("assignments").value;
+    var quizzes = document.getElementById("quizzes").value;
+    var groupproj = document.getElementById("groupproj").value;
+    var exams = document.getElementById("exams").value;
+    var intex = document.getElementById("intex").value;
+
+    var finalpercent;
+    var lettergrade;
+
+    // calculate the final percent
+    finalpercent = (assignments * .5) + (quizzes * .1) + (groupproj * .1) + (exams * .2) + (intex * .1);
+
+    //calculate the letter grade using an if statement
+    if (finalpercent >= 94) {
+        lettergrade = "A"
+    }
+    else if (finalpercent >= 90 && finalpercent < 94) {
+        lettergrade = "A-"
+    }
+    else if (finalpercent >= 87 && finalpercent < 90) {
+        lettergrade = "B+"
+    }
+    else if (finalpercent >= 84 && finalpercent < 87) {
+        lettergrade = "B"
+    }
+    else if (finalpercent >= 80 && finalpercent < 84) {
+        lettergrade = "B-"
+    }
+    else if (finalpercent >= 77 && finalpercent < 80) {
+        lettergrade = "C+"
+    }
+    else if (finalpercent >= 74 && finalpercent < 77) {
+        lettergrade = "C"
+    }
+    else if (finalpercent >= 70 && finalpercent < 74) {
+        lettergrade = "C-"
+    }
+    else if (finalpercent >= 67 && finalpercent < 70) {
+        lettergrade = "D+"
+    }
+    else if (finalpercent >= 64 && finalpercent < 67) {
+        lettergrade = "D"
+    }
+    else if (finalpercent >= 60 && finalpercent < 64) {
+        lettergrade = "D-"
+    }
+    else {
+        lettergrade = "E"
+    }
+
+    // alert the letter grade and the final percent
+    alert("Letter Grade: " + lettergrade +", Percent: " + finalpercent)
+});
